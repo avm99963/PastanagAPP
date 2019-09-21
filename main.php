c527ac0cbdaa7c2866334e960c13c3bdf289f7cc
avm99963 <jocdeladria@gmail.com>
2019-09-22 01:30:39 +0200

    Minor bug fixes

--- main.php
+++ main.php
@@ -8,45 +8,45 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Pàgina de l'usuari</title>
-		
+
 		<link rel="stylesheet" href="./css/basic.css" />
 		<link rel="stylesheet" href="./css/main.css" />
-	
+
 		<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 		<script src="https://rawgit.com/notifyjs/notifyjs/master/dist/notify.js"></script>
-		
+
 		<script src="./js/utils.js"></script>
 		<script src="./js/animations.js"></script>
-		
-		<?php 
+
+		<?php
 			require './credentials.php';
 			require './php/utils.php';
 			$user = get_users($_COOKIE['user']);
 			$victim = get_users($user->quimata);
 			if ($user->mort) header("Location: ./dead.php");
 		?>
-		
+
 		<script>
 			let user = {
 				'id': <?=$user->id?>,
 				'quimata': <?=$user->quimata?>,
 				'requested': <?=$user->requested?>,
 				'mort': <?=$user->mort?>,
-				
+
 				'nom': "<?=$user->nomcomplet?>",
 				'curs': <?=$user->curs?>,
 				'grau': <?=$user->grau?>
 			};
 		</script>
-	
+
 	</head>
 	<body>
 		<div id="outter-container">
 			<div id="inner-container">
 				<h2>Hola <name id="user_name"><?=$user->nom()?></name>,</h2>
-				
+
 				<div class="formulari_contrasenya" style="display: none;">
-					<p>Sembla que no tens contrasenya, la gent podrà entrar a la teva compta...</p>
+					<p>Sembla que no tens contrasenya, la gent podrà entrar al teu compte...</p>
 					<form action="./php/change_password.php" method="POST">
 						<input type="hidden" value="<?=$_COOKIE['user']?>" name="userid">
 						<input type="password" placeholder="Nova contrasenya..." name="password" /><br />
@@ -54,9 +54,9 @@
 						<input type="submit">
 					</form>
 				</div>
-				
+
 				<h3>La teva víctima és:</h3>
-				
+
 				<div class="victima">
 					<img width="300px" src="./imgs/<?=$victim->id?>.png" />
 					<h2 id="victim_name"><?=$victim->nomcomplet?></h2>
@@ -67,7 +67,7 @@
 		<div id="butons" class="options">
 			<button id="win" onclick="js: send_request(user, 'REQ KILL');">L'he matat</button>
 		</div>
-		
+
 		<script>
 			$(document).ready(function() {
 				// Set interval of checking
--- php/change_password.php
+++ php/change_password.php
@@ -3,17 +3,17 @@
 	require 'utils.php';
 
 	// Check if confirmation is the same
-	if ($_POST['password'] != $_POST['confirmation']) {
+	if (!isset($_POST['password']) || $_POST['password'] != $_POST['confirmation'] || empty($_POST['password'])) {
 		header("Location: ../main.php?wrong_password=1");
 		die();
 	} else {
 		// Execute query to change password
 		$update_password = "UPDATE users SET password=\"".md5($_POST['password'])."\" WHERE id=".$_POST['userid'];
 		if(!$result = query($update_password)) header("Location: ../main.php?errordb=1");
-		
+
 		// Save 'password' to cookies
 		setcookie('password', md5($_POST['password']), time() + (86400 * 10), "/");
-		
+
 		// Go back to main page
 		header("Location: ../main.php?successpassword=1");
 	}
