db383ed6bbbe5cdaef4f05a74a7cd275f406881f
avm99963 <jocdeladria@gmail.com>
2019-09-22 02:14:42 +0200

    Redesigned several parts so it looks more aethestic both in desktop and in mobile

--- main.php
+++ main.php
@@ -8,43 +8,45 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Pàgina de l'usuari</title>
-		
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
 		<link rel="stylesheet" href="./css/basic.css" />
 		<link rel="stylesheet" href="./css/main.css" />
-	
+
 		<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 		<script src="https://rawgit.com/notifyjs/notifyjs/master/dist/notify.js"></script>
-		
+
 		<script src="./js/utils.js"></script>
 		<script src="./js/animations.js"></script>
-		
-		<?php 
+
+		<?php
 			require './credentials.php';
 			require './php/utils.php';
 			$user = get_users($_COOKIE['user']);
 			$victim = get_users($user->quimata);
 			if ($user->mort) header("Location: ./dead.php");
 		?>
-		
+
 		<script>
 			let user = {
 				'id': <?=$user->id?>,
 				'quimata': <?=$user->quimata?>,
 				'requested': <?=$user->requested?>,
 				'mort': <?=$user->mort?>,
-				
+
 				'nom': "<?=$user->nomcomplet?>",
 				'curs': <?=$user->curs?>,
 				'grau': <?=$user->grau?>
 			};
 		</script>
-	
+
 	</head>
 	<body>
 		<div id="outter-container">
 			<div id="inner-container">
 				<h2>Hola <name id="user_name"><?=$user->nom()?></name>,</h2>
-				
+
 				<div class="formulari_contrasenya" style="display: none;">
 					<p>Sembla que no tens contrasenya, la gent podrà entrar a la teva compta...</p>
 					<form action="./php/change_password.php" method="POST">
@@ -54,20 +56,26 @@
 						<input type="submit">
 					</form>
 				</div>
-				
-				<h3>La teva víctima és:</h3>
-				
-				<div class="victima">
-					<img width="300px" src="./imgs/<?=$victim->id?>.png" />
-					<h2 id="victim_name"><?=$victim->nomcomplet?></h2>
-					<h3><span id="victim_curs"><?=$victim->curs?></span>-<span id="victim_grau"><?=$victim->grau?></span></h3>
-				</div>
+
+				<p>La teva víctima és:</p>
+
+				<table class="victima">
+					<tr>
+						<td><img id="victim_img" src="./imgs/<?=$victim->id?>.png" /></td>
+						<td>
+							<div id="victim_name"><?=$victim->nomcomplet?></div>
+							<div id="victim_curs_i_grau"><span id="victim_curs"><?=$victim->curs?></span>-<span id="victim_grau"><?=$victim->grau?></span></div>
+							<div id="butons" class="options">
+								<button id="win" onclick="js: send_request(user, 'REQ KILL');">L'he matat</button>
+							</div>
+						</td>
+					</tr>
+				</table>
+
+				<div style="clear: both;"></div>
 			</div>
 		</div>
-		<div id="butons" class="options">
-			<button id="win" onclick="js: send_request(user, 'REQ KILL');">L'he matat</button>
-		</div>
-		
+
 		<script>
 			$(document).ready(function() {
 				// Set interval of checking
