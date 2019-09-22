e0438abb88713757d81045b0def99ffae291cf1c
avm99963 <jocdeladria@gmail.com>
2019-09-22 17:02:33 +0200

    Added autocomplete

--- ajax/getusers.php
+++ ajax/getusers.php
@@ -1,13 +1,13 @@
-<option selected disabled hidden>Selecciona usuari...</option>
-
 <?php
 	require '../credentials.php';
 	require '../php/utils.php';
-	$users = get_users(0);
+	$users = get_users(0, false);
 
-	foreach ($users as $user) {
-		$nopassword = $user->md5password == "" ? "nopassword" : "";
-		$mort = $user->mort ? "disabled" : "";
-		echo "<option ".$mort." class='".$nopassword."' value='".$user->id."'>".$user->nomcomplet."</option>\n";
+	foreach ($users as &$user) {
+		if ($user["mort"]) continue;
+
+		$user["nopassword"] = $user["md5password"] == "" ? "nopassword" : "";
+		unset($user["md5password"]);
 	}
-?>
+
+	echo json_encode($users);
--- php/utils.php
+++ php/utils.php
@@ -5,33 +5,33 @@
 		public $curs;
 		public $grau;
 		public $quimata;
-		
+
 		public function nom() {
 			$noms = explode(" ", $this->nomcomplet);
 			return $noms[0];
 		}
 	}
-	
+
 	function query($query) {
 		// Create connection
 		$credentials = new Credentials();
 		$conn = new mysqli($credentials->servername, $credentials->username, $credentials->password, $credentials->dbname);
 		if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
 		$conn->set_charset("utf8");
-		
+
 		// Execute query and save result
 		$result = $conn->query($query);
-		
-		// Close the connection 
+
+		// Close the connection
 		$conn->close();
-		
+
 		// Return result of query
 		return $result;
 	}
-	
-	function get_users($id = 0) {
+
+	function get_users($id = 0, $getAsObjects = true) {
 		$users = [];
-		
+
 		// Prepare the query
 		$query = "SELECT * FROM users";
 		if ($id > 0) $query .= " WHERE id=".$id;
@@ -39,24 +39,37 @@
 		// Fetch the information of the user
 		if ($result = query($query)) {
 			while ($row = $result->fetch_row()) {
-				$user = new User();
-				
-				$user->id = $row[0];
-				$user->nomcomplet = $row[1];
-				$user->curs = $row[2];
-				$user->grau = $row[3];
-				$user->quimata = $row[4];
-				$user->requested = $row[5];
-				$user->mort = $row[6];
-				$user->md5password = $row[7];
-				
+				if ($getAsObjects) {
+					$user = new User();
+
+					$user->id = $row[0];
+					$user->nomcomplet = $row[1];
+					$user->curs = $row[2];
+					$user->grau = $row[3];
+					$user->quimata = $row[4];
+					$user->requested = $row[5];
+					$user->mort = $row[6];
+					$user->md5password = $row[7];
+				} else {
+					$user = [];
+
+					$user["id"] = $row[0];
+					$user["nomcomplet"] = $row[1];
+					$user["curs"] = $row[2];
+					$user["grau"] = $row[3];
+					$user["quimata"] = $row[4];
+					$user["requested"] = $row[5];
+					$user["mort"] = $row[6];
+					$user["md5password"] = $row[7];
+				}
+
 				array_push($users, $user);
 			}
 			$result->close();
 		} else {
 			die("Query failed: " . $query);
 		}
-		
+
 		if ($id > 0) return $users[0];
 		else return $users;
 	}
